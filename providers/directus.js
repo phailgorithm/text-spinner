c6cc03f0393ff039891feb418ac6e4827eed3cf6
Mohsin-Ul-Islam <mohsinulislam180@gmail.com>
2022-05-03 17:43:32 +0500

    format code via prettier

--- providers/directus.js
+++ providers/directus.js
@@ -1,4 +1,4 @@
-const { Directus } = require("@directus/sdk");
+const { Directus } = require('@directus/sdk');
 
 // treating the directus instance as a singleton
 const directus = new Directus(process.env.DIRECTUS_URL, {
